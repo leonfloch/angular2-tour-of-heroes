91df966a3185207bc72e41307505917ebd07a955
leo <80221940@OFP080196.BOLIVAR.COM>
2016-08-01 16:34:08 -0500

    commit initial

--- src/app/angular2-tour-of-heroes.component.ts
+++ src/app/angular2-tour-of-heroes.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 
 @Component({
   moduleId: module.id,
   selector: 'angular2-tour-of-heroes-app',
   templateUrl: 'angular2-tour-of-heroes.component.html',
-  styleUrls: ['angular2-tour-of-heroes.component.css']
+  styleUrls: ['angular2-tour-of-heroes.component.css'],
+  directives: [HeroDetailComponent]
 })
 export class Angular2TourOfHeroesAppComponent {
-  title = 'angular2-tour-of-heroes works!';
+  title = 'Tour de Heros';  
+  
+  selectedHero:Hero;
+  public heroes = HEROES;
+
+  /**
+   * Hero seleccionado
+   */
+  onSelect(hero:Hero) {
+    this.selectedHero = hero;
+  }
 }
